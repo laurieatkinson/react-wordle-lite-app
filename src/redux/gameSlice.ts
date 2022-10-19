0169e972fbdbbf41147e2ce420e6c43ded447c7e
Laurie Atkinson <laurieat@microsoft.com>
2022-10-19 17:09:36 -0600

    add comments

--- src/redux/gameSlice.ts
+++ src/redux/gameSlice.ts
@@ -1,7 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { getSecretWord } from '../utils/word-helpers';
-
 interface IGameState {
     wordList: string[];
     loading: boolean;
@@ -50,6 +49,7 @@ const gameSlice = createSlice({
     }
 });
 
+// Actions
 export const {
     requestStart,
     clearGame,
@@ -59,6 +59,10 @@ export const {
     getWordListFailure
 } = gameSlice.actions;
 
+// The function below is called a thunk and allows us to perform async logic.
+// It can be dispatched like a regular action: `dispatch(getWordList())`.
+// This will call the thunk with the `dispatch` function as the first argument.
+// Async code can then be executed and other actions can be dispatched
 export const getWordList = () => async (dispatch: any) => {
     dispatch(requestStart());
     await axios.get('word-list.json')
